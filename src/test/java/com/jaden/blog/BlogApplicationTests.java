8d221ab51821fe18f339eb572638511fad8d4745
Jaden <juhnmayer@gmail.com>
2022-08-28 21:09:20 +0900

    build: application.yml

--- src/test/java/com/jaden/blog/BlogApplicationTests.java
+++ src/test/java/com/jaden/blog/BlogApplicationTests.java
@@ -2,8 +2,10 @@ package com.jaden.blog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Controller;
 
 @SpringBootTest
+@Controller
 class BlogApplicationTests {
 
     @Test
